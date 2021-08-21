e2d7d192a8080c64fc878b241bf84464557de851
Tianqing <ztq0905@163.com>
2021-08-21 10:20:01 +0800

    final version

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,10 @@
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production'
-  // ? '/resume/'
-  // : '/'  
-  publicPath: './',  
   // 输出文件目录
+  publicPath: './',  
+  //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
   outputDir: 'dist',
   // eslint-loader 是否在保存的时候检查
   lintOnSave: true,
-  // use the full build with in-browser compiler?
-  // https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
-  // compiler: false,
-  // webpack配置
-  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-  chainWebpack: () => {},
-  configureWebpack: () => {},
   // 生产环境是否生成 sourceMap 文件
   productionSourceMap: true,
   // css相关配置
@@ -26,11 +17,5 @@ module.exports = {
    loaderOptions: {},
    // 启用 CSS modules for all css / pre-processor files.
    modules: false
-  },
-  // use thread-loader for babel & TS in production build
-  // enabled by default if the machine has more than 1 cores
-  parallel: require('os').cpus().length > 1,
-  // PWA 插件相关配置
-  // see https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
-  pwa: {},
+  }
 }
