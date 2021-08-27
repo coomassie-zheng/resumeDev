8da9e1994549de6e7af5ad1cce2a87e45cf1f25b
Tianqing <ztq0905@163.com>
2021-08-27 21:25:34 +0800

    final version

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -2,37 +2,53 @@
   <div>
     <p class="title">专业技能</p>
     <div id="techStack" class="box">
-      <p class="domText">计算机基础</p>
-      <p class="subText">了解基本的数据结构与算法</p>
-      <hr />
-      <p class="domText">HTML CSS</p>
-      <p class="subText">了解HTML5、CSS3新特性，可以实现基础网页布局</p>
-      <hr />
-      <p class="domText">javascript</p>
-      <p class="subText">熟悉es6部分新特性</p>
-      <hr />
-      <p class="domText">Vue.js</p>
-      <p class="subText">
-        具有vue核心、vuex、vue-router的使用经验，了解vue框架基本知识
-      </p>
-      <hr />
-      <p class="domText">前后端通信技术</p>
-      <p class="subText">了解原生ajax、axios框架、fetch方法</p>
-      <hr />
-      <p class="domText">开发辅助工具</p>
-      <p class="subText">了解webpack基本配置，熟悉git、npm的基本指令</p>
+      <div v-for="(skill, index) of skills" :key="index" >
+        <p class="domText">{{skill.title}}</p>
+        <p class="subText">{{skill.content}}</p>
+        <hr v-if="index !== 5"/>
+      </div> 
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Profile",
+  name: 'Profile',
+  data() {
+    return {
+      skills: [
+        {
+          title: '计算机基础',
+          content: '了解基本的数据结构与算法',
+        },
+        {
+          title: 'HTML CSS',
+          content: '了解HTML5、CSS3新特性，可以实现基础网页布局',
+        },
+        {
+          title: 'JavaScript',
+          content: '熟悉JavaScript基础知识，熟悉es6部分新特性',
+        },
+        {
+          title: 'Vue.js',
+          content: '具有vue核心、vuex、vue-router的基础知识',
+        },
+        {
+          title: '前后端通信技术',
+          content: '了解原生ajax、axios、fetch方法',
+        },
+        {
+          title: '开发辅助工具',
+          content: '具备git、npm的使用经验',
+        },
+      ],
+    }
+  },
 }
 </script>
 
-<style>
-@import url("../../assets/css/page.css");
+<style scoped>
+@import url('../../assets/css/page.css');
 #techStack {
   padding: 10px 14px;
 }
