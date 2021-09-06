70f8850a13e1f9be386a6bb76ca7fa6abd554e8c
Tianqing <ztq0905@163.com>
2021-09-06 20:54:42 +0800

    v-for

--- src/components/MainTabBar.vue
+++ src/components/MainTabBar.vue
@@ -1,37 +1,15 @@
 <template>
   <div>
     <TabBar>
-      <TabBarItem path="/basic">
+      <TabBarItem v-for="(page, index) of pages" :key="index" :path="page.path">
         <template v-slot:item-img>
-          <img src="../assets/img/basic.svg" alt="" />
+          <img :src="page.imgSrcs[0]" alt="" />
         </template>
         <template v-slot:item-img-active>
-          <img src="../assets/img/basic_active.svg" alt="" />
+          <img :src="page.imgSrcs[1]" alt="" />
         </template>
         <template v-slot:item-text>
-          <div>基本信息</div>
-        </template>
-      </TabBarItem>
-      <TabBarItem path="/profile">
-        <template v-slot:item-img>
-          <img src="../assets/img/profile.svg" alt="" />
-        </template>
-        <template v-slot:item-img-active>
-          <img src="../assets/img/profile_active.svg" alt="" />
-        </template>
-        <template v-slot:item-text>
-          <div>专业技能</div>
-        </template>
-      </TabBarItem>
-      <TabBarItem path="/contact">
-        <template v-slot:item-img>
-          <img src="../assets/img/contract.svg" alt="" />
-        </template>
-        <template v-slot:item-img-active>
-          <img src="../assets/img/contract_active.svg" alt="" />
-        </template>
-        <template v-slot:item-text>
-          <div>联系方式</div>
+          <div>{{ page.text }}</div>
         </template>
       </TabBarItem>
     </TabBar>
@@ -39,12 +17,41 @@
 </template>
 
 <script>
-import TabBar from "./tabbar/TabBar.vue"
-import TabBarItem from "./tabbar/TabBarItem.vue"
+import TabBar from './tabbar/TabBar.vue'
+import TabBarItem from './tabbar/TabBarItem.vue'
+//解决img动态绑定src问题
+import basicSvg from '../assets/img/basic.svg'
+import basicActiveSvg from '../assets/img/basic_active.svg'
+import profileSvg from '../assets/img/profile.svg'
+import profileActiveSvg from '../assets/img/profile_active.svg'
+import contactSvg from '../assets/img/contact.svg'
+import contactActiveSvg from '../assets/img/contact_active.svg'
+
 export default {
+  data() {
+    return {
+      pages: [
+        {
+          path: '/basic',
+          imgSrcs: [basicSvg, basicActiveSvg],
+          text: '基本信息',
+        },
+        {
+          path: '/profile',
+          imgSrcs: [profileSvg, profileActiveSvg],
+          text: '专业技能',
+        },
+        {
+          path: '/contact',
+          imgSrcs: [contactSvg, contactActiveSvg],
+          text: '联系方式',
+        },
+      ],
+    }
+  },
   components: {
     TabBarItem,
     TabBar,
-  }
+  },
 }
 </script>
